a3b772e9ef5ce4ce1ab6e21e29c46dfa5897983e
Кулиевич Артем Олексеевич <belbogK@gmail.com>
2026-05-09 17:38:33 +0300

    UpdatedViewForTracket

--- GoalTracker.Shared/Enums/CurrentStatus.cs
+++ GoalTracker.Shared/Enums/CurrentStatus.cs
@@ -3,11 +3,12 @@
     public enum CurrentStatus
     {
         Analyzing = 0,
-        Planning = 1,
-        Active = 2,
-        Cancel = 3,
-        Done = 4,
+        NotStarted = 1,
+        InProgress = 2,
+        OnHold = 3,
+        Completed = 4,
         Achieved = 5,
-        MaybeInOneDay =6
+        Cancelled= 6,
+        MaybeInOneDay =7
     }
 }
